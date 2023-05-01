a915277fa6081882dfd838bab08cba5aa1db82c5
aozp73 <aozp73@naver.com>
2023-05-01 16:45:22 +0900

    initial commit

---
+++ src/main/java/shop/mtcoding/admin_chart_sample/AdminChartSampleApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.admin_chart_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminChartSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminChartSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/admin_chart_sample/AdminChartSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.admin_chart_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminChartSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
